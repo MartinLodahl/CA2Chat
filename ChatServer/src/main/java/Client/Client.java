1b063a6fc6a615bc449abcfa23e75baba9c2abf3
Martin <cphma368@cphbusiness.dk>
2017-10-03 10:41:48 +0200

    Chat

---
+++ ChatServer/src/main/java/Client/Client.java
@@ -0,0 +1,57 @@
+package Client;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    private static final String host = "127.0.0.1";
+    private static final int portNumber = 4444;
+
+    private String userName;
+    private String serverHost;
+    private int serverPort;
+
+    public static void main(String[] args) {
+        String readName = null;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please input username:");
+        while (readName == null || readName.trim().equals("")) {
+            readName = scan.nextLine();
+            if (readName.trim().equals("")) {
+                System.out.println("Invalid. Please enter again:");
+            }
+        }
+
+        Client client = new Client(readName, host, portNumber);
+        client.startClient(scan);
+    }
+
+    private Client(String userName, String host, int portNumber) {
+        this.userName = userName;
+        this.serverHost = host;
+        this.serverPort = portNumber;
+    }
+
+    private void startClient(Scanner scan) {
+        try {
+            Socket socket = new Socket(serverHost, serverPort);
+            Thread.sleep(1000);
+
+            ServerThread serverThread = new ServerThread(socket, userName);
+            Thread serverAccessThread = new Thread(serverThread);
+            serverAccessThread.start();
+            while (serverAccessThread.isAlive()) {
+                if (scan.hasNextLine()) {
+                    serverThread.addNextMessage(scan.nextLine());
+                }
+            }
+        } catch (IOException ex) {
+            System.err.println("Fatal Connection error!");
+            ex.printStackTrace();
+        } catch (InterruptedException ex) {
+            System.out.println("Interrupted");
+        }
+    }
+}
